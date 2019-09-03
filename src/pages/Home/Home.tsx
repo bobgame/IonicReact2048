3d78d7adf0e63fb64a0814fca3127d7f559f7acd
Bob Liu <liulingbo@foxmail.com>
2019-09-03 17:31:28 +0800

    add bg

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom';
-import { Button } from 'antd';
+import { Link } from 'react-router-dom'
+import { Button } from 'antd'
 
 // const HomePage: React.Component = () => {
 const HomePage: React.FC = () => {
@@ -10,19 +10,19 @@ const HomePage: React.FC = () => {
     console.log('useEffect')
     return function cleanup() {
       console.log('useEffect out')
-    };
-  });
+    }
+  })
 
   return (
     <>
-      <h1 className="text-center mbh-4">猜数字</h1>
-      <Link to="/play"><Button size="large" type="primary" block className="mbh-2"> 猜数字 </Button></Link>
-      <Link to="/setting"><Button size="large" block className="mbh-2"> 设置 </Button></Link>
-      <Link to="/help"><Button size="large" block className="mbh-2"> 帮助 </Button></Link>
-      <Link to="/about"><Button size="large" block> 关于 </Button></Link>
+      <h1 className='text-center mbh-4'>React 2048</h1>
+      <Link to='/play'><Button size='large' type='primary' block={true} className='mbh-2'> 2048 </Button></Link>
+      <Link to='/setting'><Button size='large' block={true} className='mbh-2'> 设置 </Button></Link>
+      <Link to='/help'><Button size='large' block={true} className='mbh-2'> 帮助 </Button></Link>
+      <Link to='/about'><Button size='large' block={true}> 关于 </Button></Link>
     </>
-  );
-};
+  )
+}
 
 const stateToProps = (state: any) => {
   return {
--- src/pages/Play/Play.tsx
+++ src/pages/Play/Play.tsx
@@ -1,15 +1,20 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 
+import {
+  // useSwipeable,
+  Swipeable
+} from 'react-swipeable'
+
 import { Link } from 'react-router-dom'
 import { Button, List } from 'antd'
 
 import './Play.scss'
 import { actionClickGuess, actionClickNumber, actionDeleteNumber, actionResetList, actionCloseGameover, actionAddUseTime } from '../../store/action-creator'
-import PlayResultBox from './ui/play-ui-result-box'
-import PlayShowBox from './ui/play-ui-show-box'
-import PlayNumberBox from './ui/play-ui-number-box'
-import PlayButtonBox from './ui/play-ui-button-box'
+// import PlayResultBox from './ui/play-ui-result-box'
+// import PlayShowBox from './ui/play-ui-show-box'
+// import PlayNumberBox from './ui/play-ui-number-box'
+// import PlayButtonBox from './ui/play-ui-button-box'
 import GameoverModal from './ui/play-ui-gameover-modal'
 
 const PlayPage: React.FC = (props: any) => {
@@ -18,30 +23,44 @@ const PlayPage: React.FC = (props: any) => {
     if (props.list.length === 0) {
       props.resetLists()
     }
-    let addUseTimeInterval = setInterval(() => {
+    const addUseTimeInterval = setInterval(() => {
       props.addUseTime()
     }, 1000)
     return function cleanup() {
       clearInterval(addUseTimeInterval)
-    };
-  });
+    }
+  })
 
   const {
-    inputValue, list, times, gameover, useTime,
-    clickNumber, clickGuess, deleteNumber, closeGameOver
+    // inputValue, list,
+    times, gameover, useTime,
+    // clickNumber, clickGuess, deleteNumber,
+    closeGameOver
   } = props
 
   const data = [
     `剩余次数: ${times}`,
     `时间: ${useTime.mm}:${useTime.ss}`,
   ]
+
+  const createEmptyBgDivs = () => {
+    const divs = []
+    for (let i = 1; i <= 16; i++) {
+      divs.push(i)
+    }
+    return divs.map(d => {
+      return <div className='bg-empty-item' key={'bg-empty-' + d}>{d}</div>
+    })
+  }
+  const emptyBgDivs = createEmptyBgDivs()
+
   return (
     <>
-      <h2 className="text-center">猜数字游戏</h2>
+      <h2 className='text-center'>2048</h2>
       <List
         grid={{ column: 2 }}
-        itemLayout="vertical"
-        className="info-box mbh-2"
+        itemLayout='vertical'
+        className='info-box mbh-2'
         bordered
         dataSource={data}
         renderItem={item => (
@@ -50,14 +69,10 @@ const PlayPage: React.FC = (props: any) => {
           </List.Item>
         )}
       />
-      <PlayResultBox list={list} />
-      <PlayShowBox inputValue={inputValue} />
-      <PlayNumberBox clickNumber={clickNumber} />
-      <PlayButtonBox
-        deleteNumber={deleteNumber}
-        clickGuess={clickGuess}
-      />
-      <Link to="/"><Button block>返回首页</Button></Link>
+      <Swipeable className='play-content mbh-4' onSwipedLeft={(event) => console.log(event)}>
+        <div className='bg-empty'>{emptyBgDivs}</div>
+      </Swipeable>
+      <Link to='/'><Button block>返回首页</Button></Link>
       <GameoverModal
         gameover={gameover}
         closeGameOver={closeGameOver}
